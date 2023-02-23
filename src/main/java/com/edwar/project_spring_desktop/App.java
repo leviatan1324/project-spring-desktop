c7176668381f0a97aa1aa9ffe42f3582352fb1c6
leviatan1324 <alonsoeduardo.1324@gmail.com>
2023-02-23 14:06:43 -0600

    version inicial del proyecto project-spring-desktop

---
+++ src/main/java/com/edwar/project_spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.edwar.project_spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
